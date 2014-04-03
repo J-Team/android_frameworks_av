5b367499bba04cc434cc8c7cabaafeac76e2d2e5
Kim Zhang <xiazhang@codeaurora.org>
2014-04-03 13:07:22 +0800

    StagefrightRecorder: Pause audio encoder on recorder pause
      When the recorder is paused, OMXCodec-based audio encoders will wait
      for output buffers, timing out after 3 seconds. Pause the audio encoder
      to avoid a timeout error.
    
    CRs-Fixed: 634301
    Change-Id: I99b95bf356f87e9c18c763c542cbfcc13bc54fb1

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -4251,6 +4251,8 @@ status_t OMXCodec::start(MetaData *meta) {
 
     if(mPaused && mIsEncoder) {
         CODEC_LOGV("resume : S");
+        //wake waitForBufferFilled_l() to avoid timeout when mPause becomes false
+        mBufferFilled.signal();
         mPaused = false;
 
         if (mIsVideo) {
